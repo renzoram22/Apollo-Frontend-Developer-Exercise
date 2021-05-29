7bbfd46fd31824bc53b4b123ecba89f689185969
Captain Code <69159900+renzoram22@users.noreply.github.com>
2021-05-29 16:19:26 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+//Global variables
+var selectedType;
+var validationCode;
+// function that clean all if the user select the negative option
+
 function UpdateResponse(value) {
   if (value === 1) {
     document.getElementById('secondStepContainer').classList.remove("Hide");
@@ -9,6 +15,13 @@ function UpdateResponse(value) {
     document.getElementById('firstStepNumber').style.setProperty('color', "#00c221");
 
   }else {
+    document.getElementById('secondStepNumber').innerHTML = "";
+    document.getElementById('secondStepNumber').innerHTML = "2";
+    document.getElementById('secondStepNumber').style.setProperty('background-color', "#00c221");
+    document.getElementById('secondStepNumber').style.setProperty('color', "white");
+    document.getElementById('thirdStepContainer').classList.add("Hide");
+    document.getElementById('thirdStepNumber').classList.add("Hide");
+    document.getElementById('thirdStepLine').setAttribute("height", "0px");
     document.getElementById('secondStepContainer').classList.add("Hide");
     document.getElementById('secondStepNumber').classList.add("Hide");
     document.getElementById('secondStepLine').setAttribute("height", "0px");
@@ -19,6 +32,8 @@ function UpdateResponse(value) {
   }
 }
 
+// Generate a code and the depend of which option de user select the text change
+
 function sendCode() {
   generateCode();
   var radioCode = document.getElementsByName('Method');
@@ -28,13 +43,13 @@ function sendCode() {
             selectedMethod = e.value;
             selectedType = e.id;
             switch (selectedType) {
-              case "sms":
+              case "SMS":
                 selectedMessage = "An SMS has been sent to your mobile phone.<br> Please check your messages ("+selectedMethod+") to obtain the validation code."
                 break;
-              case "voice":
+              case "VoiceCall":
               selectedMessage = "You are going to receive a voice call.<br>Please answer the voicecall ("+selectedMethod+") to obtain the validation code."
                 break;
-              case "email":
+              case "Email":
               selectedMessage = "An email has been sent to your mail box.<br> Please check your email ("+selectedMethod+") to obtain the validation code."
                 break;
             }
@@ -52,15 +67,18 @@ secondStepNumber.style.setProperty('color', "#00c221");
 document.getElementById('alertMessage').innerHTML = selectedMessage;
 }
 
-var validationCode;
+// Generate a number code and save in a variable
+
+
 function generateCode(){
   validationCode = Math.floor(Math.random() * 1000000000000)
   document.getElementById('validationCodeNumber').innerHTML = validationCode;
   document.getElementById('VerifyButton').classList.remove("Disable");
+  document.getElementById('VerifyButton').disabled = false;
   var modal = document.getElementById("CodeModal");
   modal.style.display = "block";
   var btn = document.getElementById("myBtn");
-  var span = document.getElementsByClassName("close")[0];
+  var span = document.getElementsByClassName("Close")[0];
 
   span.onclick = function() {
     modal.style.display = "none";
@@ -72,6 +90,8 @@ function generateCode(){
     }
   }
 }
+
+// Verify everytime the user type in the code input to check if is all number or not
 
 function verifyInput(){
   var val = document.getElementById('inputCode').value;
@@ -91,14 +111,17 @@ function verifyInput(){
   }
 }
 
+// Verify if the code is the same that we generate
 
 function verifyCode(){
   var val = parseFloat(document.getElementById('inputCode').value);
   if (val === validationCode) {
-  document.getElementById('Mask').innerHTML = ''
-  document.getElementById('Mask').innerHTML = '<div class="Result"><img src="comprobado.png" alt=""> Verification completed successfully!</div>'
+  document.getElementById('Mask').innerHTML = '';
+  document.getElementById('Mask').innerHTML = '<div class="Result"><img src="comprobado.png" alt=""> Verification completed successfully!<br>1. I give my consent<br>2.'+selectedType+'<br>3.'+validationCode+'.</div>';
+
   }else {
-    document.getElementById('AlertBox').innerHTML = '<span class="closebtn" onclick="this.parentElement.style.display=`none`;">&times;</span>Wrong code. Please try again';
+    document.getElementById('AlertBox').style.display = "block";
+    document.getElementById('AlertBox').innerHTML = '<span class="Close" onclick="this.parentElement.style.display=`none`;">&times;</span>Wrong code. Please try again';
     document.getElementById('AlertBox').classList.add("AlertBox");
   }
 
